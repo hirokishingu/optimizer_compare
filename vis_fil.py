7b39a6d2c6b6616e571c190f7df84c6a3647fb19
新宮広輝 <hrksng@hrksng-no-MacBook-Pro.local>
2017-02-16 20:19:54 +0900

    vis_fil修正

--- vis_fil.py
+++ vis_fil.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from simple_convnet import SimpleConvNet
 
 def filter_show(filters, nx=8, margin=3, scale=10):
-  FN, C, FH, FW = filtrs.shape
+  FN, C, FH, FW = filters.shape
   ny = int(np.ceil(FN / nx))
   fig = plt.figure()
   fig.subplots_adjust(left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
